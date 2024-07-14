3e2ad73fd3b597281fe33b83a4ce81d7a184bc89
shivam srivastava <srivastva2006@gmail.com>
2024-07-14 16:46:50 +0530

    new changes

--- Ecommerce/store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from store.models import Product, Tag, Category
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Tag)
-admin.site.register(Category)
